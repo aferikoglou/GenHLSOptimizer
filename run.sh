5caf3bb305eb4a6dc5958e597878493182ebccea
aferikoglou <aferikoglou@gmail.com>
2022-06-13 16:55:01 +0300

    Added db analyzer to run.sh script

--- run.sh
+++ run.sh
@@ -10,19 +10,26 @@ cp ./dataset/kernels/$APP/*.cpp .
 cp ./dataset/kernels/$APP/*.h .
 cp ./dataset/kernels/$APP/*.txt .
 
+# DSE
+
 INPUT_SOURCE_PATH=./*.cpp
 INPUT_SOURCE_INFO_PATH=./kernel_info.txt
 DB_NAME=$APP
 
-python3 automatic_optimizer.py --INPUT_SOURCE_PATH $INPUT_SOURCE_PATH --INPUT_SOURCE_INFO_PATH $INPUT_SOURCE_INFO_PATH --DB_NAME $DB_NAME --GENERATIONS 24 --THREADS 40 --TIMEOUT 7200
+TIMEOUT=7200
+
+python3 automatic_optimizer.py --INPUT_SOURCE_PATH $INPUT_SOURCE_PATH --INPUT_SOURCE_INFO_PATH $INPUT_SOURCE_INFO_PATH --DB_NAME $DB_NAME --GENERATIONS 24 --THREADS 40 --TIMEOUT $TIMEOUT
+
+# DB Analysis
+
+python3 db_analyzer.py --DB_NAME $APP --TIMEOUT $TIMEOUT
 
 # Clean up
 
+# rm -r GENETIC_DSE_*
 # rm vitis_*.log
 # rm script_*.tcl
 # rm kernel_*.cpp
-# rm -r GENETIC_DSE_*
-
-rm *.cpp
-rm *.h
-rm kernel_info.txt
+# rm *.cpp
+# rm *.h
+# rm kernel_info.txt
